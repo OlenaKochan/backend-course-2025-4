b4464c3e9e08ec62396f09c3cd1206682f45fb8a
Olena Kochan bc2025-4 <olenakocan75@gmail.com>
2025-10-20 23:35:30 +0300

    Оновила файли index.js, package.json, package-lock.json.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { program } = require('commander');
 const fs = require('fs');
 const http = require('http');
+const {XMLBuilder} = require('fast-xml-parser');
+const url = require('url');
 
 program
     .option('-i, --input <path>', 'path to input file')
@@ -29,12 +31,57 @@ if(!options.port) {
     console.error('Please, specify port parameter');
     process.exit(1);
 }
-
-const data = fs.readFileSync(options.input, 'utf-8');
+//////////////////////////////////////////////////////
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(data);
+    const query = url.parse(req.url, true).query;
+
+    fs.readFile(options.input, 'utf-8', (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Error reading input file');
+            returt;
+        }
+        
+    const lines = data.trim().split('\n'); 
+    
+    let flights = [];
+    try {
+        flights = lines.map(line => JSON.parse(line)); 
+    } catch (e) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Invalid JSON line format');
+        return;
+    }
+    
+    let filteredFlights = flights;
+
+    if (query.airtime_min) {
+      const minAirTime = Number(query.airtime_min);
+      filteredFlights = filteredFlights.filter(f => f.AIR_TIME && Number(f.AIR_TIME) > minAirTime);
+    }
+
+    const showDate = query.date === 'true';
+
+    const xmlObj = {
+      flights: {
+        flight: filteredFlights.map(f => {
+          const obj = {};
+          if (showDate) obj.date = f.FL_DATE;
+          if (f.AIR_TIME) obj.air_time = f.AIR_TIME;
+          if (f.DISTANCE) obj.distance = f.DISTANCE;
+          return obj;
+        }),
+      },
+    };
+
+    const builder = new XMLBuilder({ format: true });
+    const xml = builder.build(xmlObj);
+
+    res.writeHead(200, { 'Content-Type': 'application/xml' });
+    res.end(xml);
+  });
+
 });
 
 server.listen(Number(options.port), options.host, () => {
